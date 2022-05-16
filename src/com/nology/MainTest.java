29d5f3c9e692714fb62b7a3a4c38d23c5ed12c7d
Ommote <olivia.motevalli@gmail.com>
2022-05-16 13:01:59 +0100

    created initial tests

---
+++ src/com/nology/MainTest.java
@@ -0,0 +1,22 @@
+package com.nology;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    @DisplayName("Translate hello")
+    void translateHello() {
+        assertEquals("hello", MorseTranslator.translate(".... . .-.. .-.. ---"));
+    }
+
+    @Test
+    @DisplayName("Translate olivia")
+    void translateOlivia() {
+        assertEquals("olivia", MorseTranslator.translate("--- .-.. .. ...- .. .-"));
+    }
+
+}
