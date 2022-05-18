a6e37e3f8c1e34d4a51d38bc0e0d3c96cdb2a722
Ommote <olivia.motevalli@gmail.com>
2022-05-18 13:01:43 +0100

    updated tests

--- src/com/nology/MainTest.java
+++ src/com/nology/MainTest.java
@@ -7,16 +7,27 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
 
-//    @Test
-//    @DisplayName("Translate hello")
-//    void translateHello() {
-//        assertEquals("hello", MorseTranslator.translate(".... . .-.. .-.. ---"));
-//    }
-//
-//    @Test
-//    @DisplayName("Translate olivia")
-//    void translateOlivia() {
-//        assertEquals("olivia", MorseTranslator.translate("--- .-.. .. ...- .. .-"));
-//    }
+    @Test
+    @DisplayName("Translate hello")
+    void translateHello() {
+        assertEquals("hello", MorseTranslator.translateMorse(".... . .-.. .-.. ---"));
+    }
 
+    @Test
+    @DisplayName("Translate olivia")
+    void translateOlivia() {
+        assertEquals("olivia", MorseTranslator.translateMorse("--- .-.. .. ...- .. .-"));
+    }
+
+    @Test
+    @DisplayName("Translate nology")
+    void translateNology() {
+        assertEquals("nology", MorseTranslator.translateMorse("-. --- .-.. --- --. -.--"));
+    }
+
+    @Test
+    @DisplayName("Translate sentence")
+    void translateSentence() {
+        assertEquals("hello friendly world", MorseTranslator.translateMorse(".... . .-.. .-.. --- / ..-. .-. .. . -. -.. .-.. -.-- / .-- --- .-. .-.. -.."));
+    }
 }
